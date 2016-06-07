c7e94d8c6782f975a67efa57a01259236f757a3d
Critical Mass Sternfahrt <criticalmasssternfahrt@users.noreply.github.com>
2016-06-07 22:32:30 +0200

    Adjusted layout for cards

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 npm install
 grunt
-tar --xform s:'./':'./themes/critical-mass-sternfahrt-theme/': -czf v0.0.8-release.tar.gz ./layouts/ ./static/ ./theme.toml
+tar --xform s:'./':'./themes/critical-mass-sternfahrt-theme/': -czf v0.0.9-release.tar.gz ./layouts/ ./static/ ./theme.toml
